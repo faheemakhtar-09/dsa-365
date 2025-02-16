61466f7047e3c530c8486c2015b20ca0b64b7cd6
Faheem Akhtar <faheem.akhtar@unthinkable.co>
2025-02-17 01:29:04 +0530

    Day two Basic of Variables and data types

---
+++ PrimitiveExample.java
@@ -0,0 +1,21 @@
+public class PrimitiveExample {
+    public static void main(String[] args) {
+        byte smallNumber = 127;
+        short shortNumber = 32000;
+        int integerNumber = 100000;
+        long longNumber = 10000000000L;
+        float floatNumber = 3.14f;
+        double doubleNumber = 3.141592653589793;
+        char letter = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Byte: " + smallNumber);
+        System.out.println("Short: " + shortNumber);
+        System.out.println("Int: " + integerNumber);
+        System.out.println("Long: " + longNumber);
+        System.out.println("Float: " + floatNumber);
+        System.out.println("Double: " + doubleNumber);
+        System.out.println("Char: " + letter);
+        System.out.println("Boolean: " + isJavaFun);
+    }
+}
